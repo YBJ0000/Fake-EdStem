15fd6ca48270fc8fbda0d210162736f06b5ba30d
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2024-10-19 21:20:20 +1100

    basic html and js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,10 @@ import { BACKEND_PORT } from './config.js';
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
 
-console.log('Let\'s go!');
+document.getElementById('register-btn').addEventListener('click', () => {
+  const email = document.getElementById('register-email').value
+  const password = document.getElementById('register-password').value
+  const name = document.getElementById('register-name').value
+
+  console.log(email, password, name);
+})
